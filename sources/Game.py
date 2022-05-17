54d9dca40dc959179d7b467c88501c748c1c61cf
roganOhDev <fdscbjdcnhd@gmail.com>
2022-05-17 21:24:55 +0900

    move files

--- sources/Game.py
+++ sources/Game.py
@@ -2,7 +2,13 @@ from dataclasses import dataclass
 
 @dataclass
 class Game:
+    character: Character
+    background: Background
+    
+    
     screen_width: int = 640
     screen_height: int = 480
     fps: int = 50
     speed: float = 5 / fps
+    
+    
